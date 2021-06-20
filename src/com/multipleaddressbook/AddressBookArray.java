a3038deca0087937cb460b0557f7153da14366a6
Aniket-123-bit <anisunny782@gmail.com>
2021-06-20 15:14:54 +0530

    Coomit Uc6 Push

---
+++ src/com/multipleaddressbook/AddressBookArray.java
@@ -0,0 +1,16 @@
+package com.multipleaddressbook;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Added AddressbookArray class to add an arraylist code in it.
+ * Here i modified the access of the Contactlist to protected.
+ * Because protected objects can be accessed in inner classes.
+ */
+
+public class AddressBookArray {
+	  Scanner sc=new Scanner(System.in);
+	    protected ArrayList<Contact> contactList = new ArrayList<Contact>();
+	    String addressBookName;
+
+}
